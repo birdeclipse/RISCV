8b7ea6db8b9a85536e8e5bac3159e17e7e5f59ff
birdeclipse <yqiu21@ucsc.edu>
2016-03-11 17:47:31 -0800

    beautify the code

--- RISCV/rtl/fetch.v
+++ RISCV/rtl/fetch.v
@@ -7,79 +7,71 @@
  * 
  * TODO: Add module documentation
  */
-/* verilator lint_off UNUSED */
 module fetch(
-	
-	input				clk,
-	input				reset,
-	
-	output				icache_ack_data_retry,	
-	input				icache_ack_data_valid,
-	input  [255:0] 		icache_ack_data,
+	     input 	     clk,
+	     input 	     reset,
 
-	output [63-5:0] 	icache_req_addr,
-	output        		icache_req_addr_valid,
-	input         		icache_req_addr_retry,	
-	
-	
-	/*branch and stall*/
-	input	[63:0]		branch_target,
-	input				branch_target_enable,
-	
-	
-	
-	/**/
-	output	[63:0]		decode_ack_pc,			
-	output	[31:0]		decode_ack_instr,		
-	output				decode_ack_data_valid,	
-	input				decode_ack_data_rety 	
+	     output 	     icache_ack_data_retry, 
+	     input 	     icache_ack_data_valid,
+	     input [255:0]   icache_ack_data,
 
+	     output [63-5:0] icache_req_addr,
+	     output 	     icache_req_addr_valid,
+	     input 	     icache_req_addr_retry, 
+	     
+	
+	     /*branch and stall*/
+	     input [63:0]    branch_target,
+	     input 	     branch_target_enable,
+	     
+	     output [63:0]   decode_ack_pc, 
+	     output [31:0]   decode_ack_instr, 
+	     output 	     decode_ack_data_valid, 
+	     input 	     decode_ack_data_rety 	
 );
 
 /* verilator lint_off UNUSED */
 
-wire[63:0]				program_counter;
-wire					fetch_req_next_pc_valid;
-wire					icache_req_next_pc_rety;
-wire[31:0]				icache_ack_instr;
-wire					icahce_ack_instr_valid;			
-wire					icache_ack_instr_rety;
+   wire [63:0]	     program_counter;
+   wire 	     fetch_req_next_pc_valid;
+   wire 	     icache_req_next_pc_rety;
+   wire [31:0]	     icache_ack_instr;
+   wire 	     icahce_ack_instr_valid;			
+   wire 	     icache_ack_instr_rety;
 
 
-reg	[63:0]				program_counter_next;
-reg	[63:0]				fetch_ack_pc_next;
-reg	[63:0]				fetch_req_next_pc;	
+   reg [63:0] 	     program_counter_next;
+   reg [63:0] 	     fetch_ack_pc_next;
+   reg [63:0] 	     fetch_req_next_pc;	
 
-assign	fetch_req_next_pc_valid	=	1'b1;							/*always valid*/
+assign	fetch_req_next_pc_valid	= 1'b1;/*always valid*/
 
 
 
 always@(*)begin
-	if(branch_target_enable)
-		begin
-			fetch_req_next_pc	=	branch_target;
-		end
-	else if(icache_req_next_pc_rety|icache_ack_instr_rety|decode_ack_data_rety)
-		begin
-			fetch_req_next_pc	=	program_counter;
-		end
-	else 
-		begin
-			fetch_req_next_pc	=	program_counter+4;
-		end
+   if(branch_target_enable)
+     begin
+	fetch_req_next_pc	=	branch_target;
+     end
+   else if(icache_req_next_pc_rety|icache_ack_instr_rety|decode_ack_data_rety)
+     begin
+	fetch_req_next_pc	=	program_counter;
+     end
+   else 
+     begin
+	fetch_req_next_pc	=	program_counter+4;
+     end
 end
 
 
 always@(*)begin
-
-	program_counter_next	=	fetch_req_next_pc;
-
+   program_counter_next	= fetch_req_next_pc;
 end
 
 
 
 always@(*)begin
-	fetch_ack_pc_next	=	program_counter;
+   fetch_ack_pc_next = program_counter;
 end
 
 
@@ -126,26 +118,17 @@ flop #(.Bits(64)) PC(
 Fluid_Flop#(.Size(96))
 DECODE(
 
-    .clk		(clk),
-    .reset		(reset),
+       .clk(clk),
+       .reset(reset),
 
-    .din		({fetch_ack_pc_next,icache_ack_instr}),
-    .dinValid	(icahce_ack_instr_valid&(~branch_target_enable)&(~decode_ack_data_rety)),
-    .dinRetry	(icache_ack_instr_rety),
+       .din({fetch_ack_pc_next,icache_ack_instr}),
+       .dinValid(icahce_ack_instr_valid&(~branch_target_enable)&(~decode_ack_data_rety)),
+       .dinRetry(icache_ack_instr_rety),
 
-    .q			({decode_ack_pc,decode_ack_instr}),
-    .qRetry		(decode_ack_data_rety),
-    .qValid		(decode_ack_data_valid)
+       .q({decode_ack_pc,decode_ack_instr}),
+       .qRetry(decode_ack_data_rety),
+       .qValid(decode_ack_data_valid)
     
-);    
+);
 
-
-
-
-
-
-
-	
-	
-	
 endmodule
